e773f0444cc6bb2ea6588c0c737a8d3fda805d11
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-07 23:40:55 +0000

    Set installer permissions correctly

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,10 +1,10 @@
 cd /home/imconfusednow/repo/django_site
 git pull
 cp -R /home/imconfusednow/repo/django_site /home/imconfusednow/cv_project
-chmod 744 -R /home/imconfusednow/cv_project
+chmod 775 -R /home/imconfusednow/cv_project
 chown www-data:imconfusednow -R /home/imconfusednow/cv_project
 python3 /home/imconfusednow/cv_project/manage.py collectstatic --noinput --clear
 server_pid=`ps aux | grep coup_server.py | grep -v grep | awk '{print $2}'`
 kill ${server_pid}
 nohup python3 /home/imconfusednow/cv_project/coup/coup_server.py&
-cd
+cd 
