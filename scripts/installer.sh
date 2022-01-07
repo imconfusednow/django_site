be7ca7c56c8e9cd0f2a175820d39c782f093f344
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-07 22:52:15 +0000

    Add installer

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,7 +3,7 @@ git pull
 chmod 744 -R /home/imconfusednow/repo
 chown 744 -R /home/imconfusednow/repo
 cp -R /home/imconfusednow/repo/django_site /home/imconfusednow/cv_project
-python /home/imconfusednow/cv_project/manage.py collectstatic --noinput --clear
+python3 /home/imconfusednow/cv_project/manage.py collectstatic --noinput --clear
 server_pid=`ps aux | grep coup_server.py | grep -v grep | awk '{print $2}'`
 kill ${server_pid}
 nohup python3 /home/imconfusednow/cv_project/coup/coup_server.py&
