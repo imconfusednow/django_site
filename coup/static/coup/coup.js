19f0f5c29e8ef419e267edeced9aff0483171278
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-10 00:32:00 +0000

    start adding game divs and images

--- coup/static/coup/coup.js
+++ coup/static/coup/coup.js
@@ -2,9 +2,9 @@ const socket = io("http://81.110.114.52:3000");
 
 socket.on('connect', () => {
     console.log('connected');
-    if (!localStorage.getItem("player_id")) localStorage.setItem("player_id", socket.id)
-    var title = document.getElementById("coup-title");
-    title.innerText = title.innerText + " " + localStorage.getItem("player_id");
+    //if (!localStorage.getItem("player_id")) localStorage.setItem("player_id", socket.id)
+    //var title = document.getElementById("coup-title");
+    //title.innerText = title.innerText + " " + localStorage.getItem("player_id");
 });
 
 socket.on('disconnect', () => {
