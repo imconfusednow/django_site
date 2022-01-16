3a37c2b58d8d585524b0bf61857c4f48a0735b7d
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-16 19:54:00 +0000

    add handling of game name

--- coup/static/coup/coup_login.js
+++ coup/static/coup/coup_login.js
@@ -1,14 +1,14 @@
 function joinGame(button)
 {
     var submit_type_input = document.getElementById("submit-type");
-    game_id_input.value = "join";
+    submit_type_input.value = "join";
     button.form.submit();
 }
 
 function createGame(button)
 {
     var submit_type_input = document.getElementById("submit-type");
-    game_id_input.value = "create";
+    submit_type_input.value = "create";
     button.form.submit();
 }
 
