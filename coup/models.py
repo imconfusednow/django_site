9713b87222f18052deab2d66096d0aae1c28f2b2
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-16 19:31:35 +0000

    add games and decks tables

--- coup/models.py
+++ coup/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 class players(models.Model):
     name = models.CharField(max_length=50, default="")
-    game_id = models.CharField(max_length=50, default="")
+    game_id = models.ForeignKey(games)
     player_id = models.CharField(max_length=50, default="")
     sequence = models.IntegerField(default=0)
     turn = models.BooleanField(default=False)
     computer = models.BooleanField(default=False)
     coins = models.IntegerField(default=0)
     hand = models.CharField(max_length=50, default="")
+
+class games(models.Model):
+    name = models.CharField(max_length=50, default="")
+
+class decks(models.Model):
+    game_id = models.ForeignKey(games)
+    card_type = models.CharField(max_length=10, default="")
+    sequence = models.IntegerField(default=0)
