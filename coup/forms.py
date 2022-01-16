8122b3283c2a6399cff425719eb585a1093a998a
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-16 20:11:45 +0000

    add handling of game name

--- coup/forms.py
+++ coup/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    game_id = forms.CharField(label="")
+    room_name = forms.CharField(label="")
     submit_type = forms.CharField(label="")
