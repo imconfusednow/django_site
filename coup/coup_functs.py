408691a75ad7918a4b8e6235df0d2b2644554a55
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-17 00:45:44 +0000

    add handling of game name

--- coup/coup_functs.py
+++ coup/coup_functs.py
@@ -13,10 +13,10 @@ def join_game(name, session_id):
     existing = games.objects.filter(name=name)
     if not existing.exists():
         return f"Room does not exist, click create to create this room: {name}"
-    add_player(existing, session_id)
+    add_player(existing.first(), session_id)
 
 def add_player(room, session_id):
-    existing = players.objects.get(session_id=session_id)
+    existing = players.objects.filter(session_id=session_id)
     pk = None
     if existing.exists():
         pk = existing[0].pk
