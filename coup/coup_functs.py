8bea94313a086a5fd7272c989be7b243c76a088f
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-19 23:29:33 +0000

    start grabbing players from db

--- coup/coup_functs.py
+++ coup/coup_functs.py
@@ -6,15 +6,16 @@ def add_game(name,player_id):
         return {"error":"Room does not exist, click create to create this room: {name}"}
     game = games(name=name)
     game.save()
-    pk = add_player(game, player_id)
-    return {"player_id":pk}
+    pk = add_player(game, player_id)    
+    return {"player_id":pk, players: []}
 
 def join_game(name, player_id):
     existing = games.objects.filter(name=name)
     if not existing.exists():
         return {"error":"Room does not exist, click create to create this room: {name}"}
+    existing_players = players.objects.filter(game_id=existing.first())
     pk = add_player(existing.first(), player_id)
-    return {"player_id":pk}
+    return {"player_id":pk, "players": existing_players}
 
 def add_player(room, player_id):
     existing = players.objects.filter(pk=player_id)
