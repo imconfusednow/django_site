fd69780a284cafc5fa3c9cf4c61ebc2ec87b42fa
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-01-21 02:30:30 +0000

    set name from nick

--- coup/coup_server_functs.py
+++ coup/coup_server_functs.py
@@ -3,5 +3,10 @@ import sqlite3
 
 
 def set_player_nick(player_id, name):
-    con = sqlite3.connect('/home/imconfusednow/cv_project/db.sqlite3')
-    con.execute("UPDATE coup_players SET name = ? WHERE id = ?", [player_id, name])
+    try:
+        con = sqlite3.connect('/home/imconfusednow/cv_project/db.sqlite3')
+        con.execute("UPDATE coup_players SET name = ? WHERE id = ?", [name, player_id])
+        con.commit()
+        con.close()
+    except Exception as e:
+        print(e)
