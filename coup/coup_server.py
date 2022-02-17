bdb8a73862191beffb5b0af97e9374b24953c66d
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-02-17 00:46:19 +0000

    send opponent hand size

--- coup/coup_server.py
+++ coup/coup_server.py
@@ -21,9 +21,7 @@ def join_game(sid, data):
     room = c.sid_to_room(sid)
     players = c.get_players(False, sid)
     sio.enter_room(sid, room)
-    for i in players:
-        sio.emit("join_game", players, to=players[0]["player_id"])
-        players.append(players.pop(0))        
+    send_info(players, sid, False, "join_game")   
     print(f"Player {sid} entered room {room}")
 
 
@@ -31,9 +29,7 @@ def join_game(sid, data):
 def start_game(sid):
     players = c.get_players(True, sid)
     c.deal(players)
-    for i in players:
-        sio.emit("start_game", players, to=players[0]["player_id"])
-        players.append(players.pop(0))        
+    send_info(players, sid, False, "start_game")
     print(f"Game {players[0]['game_id_id']} started by {sio}")
 
 
@@ -42,12 +38,22 @@ def rejoin_game(sid, data):
     c.set_player_nick(data["player_id"], sid, data["nick"])
     room = c.sid_to_room(sid)
     players = c.get_players(False, sid)
-    sio.enter_room(sid, room)
-    for i in players:
-        players.append(players.pop(0))
-        if players[0]["player_id"] == sid:
-            sio.emit("rejoin_game", players, to=players[0]["player_id"])
+    sio.enter_room(sid, room)    
+    send_info(players, sid, True, "rejoin_game")   
     print(f"Player {sid} entered room {room}")
+
+
+def send_info(players, sid, method, only_one):
+    hands = [h.pop("hand") for h in players]
+    no_cards = [len(h.split(",")) for h in hands]
+    for i in players:
+        if players[0]["player_id"] == sid:
+            sio.emit(method, players, hands[0] + no_cards[1:-1],  to=players[0]["player_id"])
+            if only_one: break
+        players.append(players.pop(0))
+        hands.append(hands.pop(0))
+        no_cards.append(no_cards.pop(0))
+
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 3000)), app)
