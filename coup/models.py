1448cc6ab487fb28f554cd3bc6373232dbad2235
Ben Allport <ben_e_allport@hotmail.co.uk>
2022-02-19 22:47:13 +0000

    add ai players

--- coup/models.py
+++ coup/models.py
@@ -4,7 +4,6 @@ class players(models.Model):
     name = models.CharField(max_length=50, default="")
     game_id = models.ForeignKey("games", on_delete=models.SET(None), null=True)
     player_id = models.CharField(max_length=50, default="")
-    sequence = models.IntegerField(default=0)
     turn = models.BooleanField(default=False)
     computer = models.BooleanField(default=False)
     coins = models.IntegerField(default=0)
@@ -17,4 +16,3 @@ class games(models.Model):
 class decks(models.Model):
     game_id = models.ForeignKey("games", on_delete=models.CASCADE)
     card_type = models.CharField(max_length=10, default="")
-    sequence = models.IntegerField(default=0)
